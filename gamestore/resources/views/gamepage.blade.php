f2d600c031d9d851ca65dd1ed650f792d1fb54e5
Student Jim van Emmerik <jemmerik4622@student.landstede.nl>
2022-10-05 14:52:26 +0200

    gamestore

---
+++ gamestore/resources/views/gamepage.blade.php
@@ -0,0 +1,23 @@
+<div style="width: 1440px; height: 1024px; position: relative;">
+    <div style="width: 1440px; height: 1024px; left: 0px; top: 0px; position: absolute; background-color: white; position: relative;">
+        <div style="width: 1440px; height: 100px; padding-left: 384px; padding-right: 14px; left: 0px; top: 0px; position: absolute; background-color: rgba(0, 16, 85, 1); display: inline-flex; flex-direction: row; align-items: center; justify-content: flex-end;">
+            <p style="width: 142px; height: 100px; font-size: 25px; line-height: 15px; text-align: center; color: white;">Store</p>
+            <div style="width: 146px;"/>
+            <p style="width: 204px; height: 100px; font-size: 25px; line-height: 15px; text-align: center; color: white;">Category</p>
+            <div style="width: 146px;"/>
+            <p style="width: 171px; height: 100px; font-size: 25px; line-height: 15px; text-align: center; color: white;">Library</p>
+            <div style="width: 146px;"/>
+            <div style="width: 87px; height: 76px; background-color: white; border-radius: 8px;"/>
+        </div>
+        <p style="width: 151px; height: 100px; left: 1209px; top: 9px; position: absolute; font-size: 25px; line-height: 15px; text-align: center; color: white;">Profile</p>
+        <div style="width: 1440px; height: 924px; left: 0px; top: 100px; position: absolute; background-color: rgba(70, 84, 144, 1); position: relative;">
+            <div style="width: 307px; height: 39px; left: 1119px; top: 16px; position: absolute; background-color: rgba(217, 217, 217, 1);"/>
+            <p style="width: 307px; height: 39px; left: 1119px; top: 16px; position: absolute; font-size: 25px; line-height: 15px; text-align: right; color: black;">Search</p>
+            <div style="width: 40px; height: 29px; left: 1124px; top: 21px; position: absolute; background-color: black; border-radius: 8px;"/>
+        </div>
+    </div>
+    <div style="width: 935px; height: 500px; left: 184px; top: 276px; position: absolute; background-color: rgba(178, 191, 248, 1); box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"/>
+    <p style="width: 282px; height: 405px; left: 1136px; top: 371px; position: absolute; font-size: 25px; line-height: 15px; color: white;">Game description</p>
+    <p style="width: 283px; height: 74px; left: 1135px; top: 277px; position: absolute; font-size: 25px; line-height: 15px; color: white;">Price van the game</p>
+    <p style="width: 249px; height: 62px; left: 192px; top: 214px; position: absolute; font-size: 25px; line-height: 15px; color: white;">Title of the game</p>
+</div>
